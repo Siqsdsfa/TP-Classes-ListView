2bd98e8027ad749b9765af3f324e66ac6c47cab5
Mateo <117085274+Siqsdsfa@users.noreply.github.com>
2024-07-11 23:39:53 -0300

    Completed TP- Classes - ListView

--- tp_classes_listview/lib/screens/home_screen.dart
+++ tp_classes_listview/lib/screens/home_screen.dart
@@ -202,6 +202,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           );
         });
+
         clearTextControllers();
         Navigator.of(context).pop();
       } else {
